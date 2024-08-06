92c616237d676d0aa29bc56c5c18da545b461bcc
nusratullo_a <axunovnusrat@gmail.com>
2024-08-06 05:45:14 +0500

    sdfsdf

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="flex flex-col gap-3 p-10">
+    <h1 class="text-3xl font-bold">
+      👋 Hello {{ $t('main.name') }} starter
+    </h1>
+    <p>
+      {{Store.sampleData}}
+    </p>
+    <button
+      @click="changeSampleStoreData('Hello from changed store!')"
+      class="px-3 py-2 w-fit bg-gray-800 text-white"
+    >
+      Change state
+    </button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useSampleStore } from '~/store/sample';
+  
+  const Store = useSampleStore();
+
+  function changeSampleStoreData(testString: string) {
+    Store.setSampleData(testString);
+  }
+
+</script>
